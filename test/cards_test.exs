a9e7c0296fcc6c71c5352e826558c137a39f55c5
assertnotnull <patgauth@gmail.com>
2018-07-11 13:52:53 -0400

    add tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,4 +2,13 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "createDeck makes 20 cards" do
+    deckLength = length(Cards.createDeck)
+    assert deckLength == 20
+  end
+
+  test "shuffling a deck reandomizes it" do
+    deck = Cards.createDeck
+    refute deck == Cards.shuffle(deck)
+  end
 end
