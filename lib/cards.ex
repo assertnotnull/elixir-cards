c032c4d676f3e1cb6f9c9616a89d3a546ae3a1aa
assertnotnull <patgauth@gmail.com>
2018-07-11 13:52:35 -0400

    remove warning

--- lib/cards.ex
+++ lib/cards.ex
@@ -18,6 +18,16 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.createDeck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -29,7 +39,7 @@ defmodule Cards do
   ## Examples
 
       iex> deck = Cards.createDeck
-      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> {hand, _} = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
 
